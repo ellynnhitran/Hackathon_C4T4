92d4a62a6264f504516d70ea26d00f177e63e9ae
MY-ENBF73M131K5\MyPC <donezombie@gmail.com>
2018-08-22 21:44:46 +0700

    .

--- app.py
+++ app.py
@@ -9,5 +9,9 @@ def home():
 def pagecon():
     return render_template("pagecon.html")
 
+@app.route("/test")
+def test():
+    return render_template("index.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
