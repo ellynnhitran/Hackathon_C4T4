385b0ec8b51c28ac3e0ec79f2fd7f2becf2589d7
MY-ENBF73M131K5\MyPC <donezombie@gmail.com>
2018-08-22 21:05:08 +0700

    new

--- populate.py
+++ populate.py
@@ -1,10 +1,14 @@
 import mlab
 from models.food import Food
 import pyexcel
-a = pyexcel.Sheet()
-print(a)
+import pandas as pd
+from pandas import ExcelWriter
+from pandas import ExcelFile
+
 mlab.connect()
+df = pd.read_excel('Crawdata.xlsx', sheet_name='pyexcel_sheet1')
 
-food = Food(title="bbbbb", img="iohoih", nguyenlieu="oijpj", cachlam="hihi")
-
-food.save()
+for i in df.index:
+  food = Food(title=df['title'][i], img=df['img'][i], nguyenlieu=df['nguyenlieu'][i].strip("\n").replace(" ",""), cachlam=df['cachlam'][i])
+  # food.save()
+print("Completed")
